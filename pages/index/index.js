22460aee631632521ea95245dd3966bee9a7e57e
hzx11010 <13641040120@163.com>
2018-07-17 20:30:54 +0800

    '首页'

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,6 +9,11 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     isTrue:false,
+    isNumber: 0,
+    last_update: 0,
+    last_x: 0,
+    last_y: 0,
+    last_z: 0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -17,46 +22,34 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
   },
   onShow:function () {
-    wx.onAccelerometerChange(function (e) {
-      console.log(e.x)
-      console.log(e.y)
-      console.log(e.z)
-      if (e.x > 1 && e.y > 1) {
-        wx.showToast({
-          title: '摇一摇成功',
-          icon: 'success',
-          duration: 2000
-        })
-      }
-    })
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -65,5 +58,53 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  onReady: function () {
+    var that = this;
+    var x = 0,y = 0,z = 0, lastX = 0, lastY= 0, lastZ = 0;
+    var shakeSpeend = 110; // 设置阀值 控制速度
+    var lastTime = 0;
+    function shake(res) {
+      console.log(1)
+      var newTime = new Date().getTime();
+      if(newTime - lastTime > 100){
+        var diffTime = newTime - lastTime;
+        lastTime = newTime;
+        x = res.x;
+        y = res.y;
+        z = res.z;
+        var speed = Math.abs(x + y + z - lastX - lastY - lastZ) / diffTime * 10000; // 记录速度
+        if (speed > shakeSpeend) {
+          // 开始请求接口来获取中奖信息
+          // wx.stopAccelerometer()
+          console.log('m')
+          // wx.playBackgroundAudio({ // 音乐
+          //   dataUrl: '',
+          //   title: '',
+          //   coverImgUrl: ''
+          // })
+          wx.showLoading({ 
+            title: '摇奖中...' 
+          }) 
+          // wx.request({
+          //   url: '',
+          //   success:function (data) {
+          //     setTimeout(function(){
+          //       wx.playBackgroundAudio({ // 音乐
+          //         dataUrl: '',
+          //         title: '',
+          //         coverImgUrl: ''
+          //       })
+          //       wx.hideLoading()
+          //     },2000)
+          //     lastX = x; //赋值，为下一次计算做准备 
+          //     lastY = y; 
+          //     lastZ = z;
+          //   }
+          // })
+        }
+      }
+    }
+    wx.onAccelerometerChange(shake)
   }
 })
