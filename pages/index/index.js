5ae1b3c917a963fc58dad2ebc4a7760ce74bc349
hzx11010 <13641040120@163.com>
2018-07-18 10:37:06 +0800

    '首页'

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,9 +8,9 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    isTrue:false,
     isNumber: 0,
     last_update: 0,
+    isTrue: false,
     last_x: 0,
     last_y: 0,
     last_z: 0
@@ -61,13 +61,16 @@ Page({
   },
   onReady: function () {
     var that = this;
-    var x = 0,y = 0,z = 0, lastX = 0, lastY= 0, lastZ = 0;
-    var shakeSpeend = 110; // 设置阀值 控制速度
-    var lastTime = 0;
+    // wx.onAccelerometerChange(shake)
+  },
+  shake: function () {
     function shake(res) {
-      console.log(1)
+      var x = 0, y = 0, z = 0, lastX = 0, lastY = 0, lastZ = 0;
+      var shakeSpeend = 110; // 设置阀值 控制速度
+      var lastTime = 0;
+      console.log(1);
       var newTime = new Date().getTime();
-      if(newTime - lastTime > 100){
+      if (newTime - lastTime > 100) {
         var diffTime = newTime - lastTime;
         lastTime = newTime;
         x = res.x;
@@ -83,20 +86,20 @@ Page({
           //   title: '',
           //   coverImgUrl: ''
           // })
-          wx.showLoading({ 
-            title: '摇奖中...' 
-          }) 
+          wx.showLoading({
+            title: '摇奖中...'
+          })
           // wx.request({
           //   url: '',
           //   success:function (data) {
-              setTimeout(function(){
-                // wx.playBackgroundAudio({ // 音乐
-                //   dataUrl: '',
-                //   title: '',
-                //   coverImgUrl: ''
-                // })
-                wx.hideLoading()
-              },2000)
+          setTimeout(function () {
+            // wx.playBackgroundAudio({ // 音乐
+            //   dataUrl: '',
+            //   title: '',
+            //   coverImgUrl: ''
+            // })
+            wx.hideLoading()
+          }, 2000)
           //     lastX = x; //赋值，为下一次计算做准备 
           //     lastY = y; 
           //     lastZ = z;
@@ -105,6 +108,5 @@ Page({
         }
       }
     }
-    wx.onAccelerometerChange(shake)
   }
 })
