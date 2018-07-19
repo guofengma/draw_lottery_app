8f2562c750502c27722724c16babcb9cff3b592b
weiweikate <690125575@qq.com>
2018-07-19 14:00:49 +0800

    修改代码

--- pages/address/address-list/address-list.js
+++ pages/address/address-list/address-list.js
@@ -6,16 +6,21 @@ Page({
       {name:"陈我IE我IE",phone:'184583221',address:'哈哈哈哈哈'}
     ],
     door:0, // 2 朵地址
+    showDefault:true,
   },
   onLoad: function (options) {
+    let showDefault = this.data.showDefault
+    if (options.door==2){
+      showDefault = false
+    }
     this.setData({
-      door: options.door || 0
+      door: options.door || 0,
+      showDefault: showDefault
     })
     this.queryUserAddressList()
     Event.on('updateAdressList', this.queryUserAddressList, this)
   },
   addressLineClicked(e){
-    
     let {door} = this.data
     let index = e.currentTarget.dataset.index
     let address = this.data.lists[index]
