e2f840352dd02ae7db83acc429a43bb43b244b8c
weiweikate <690125575@qq.com>
2018-07-24 18:10:10 +0800

    修改代码

--- network/base-requests/request.js
+++ network/base-requests/request.js
@@ -10,7 +10,7 @@ import RequestStatus from './request-status';
 export default class Request {
     getBaseUrl(bParam) {
         // 开发 
-        this.baseUrl = 'http://172.16.10.7:'
+        // this.baseUrl = 'http://172.16.10.7:'
         // this.baseUrl = 'http://172.16.10.21:'
         // this.baseUrl = 'http://172.16.10.4:'
         // this.baseUrl = 'http://172.16.10.12:'
@@ -18,13 +18,13 @@ export default class Request {
         // this.baseUrl = 'http://172.16.10.19:'
         // this.baseUrl = 'http://172.16.10.253:'
 
-        if (bParam.port) {
-            this.baseUrl = this.baseUrl + bParam.port
-        } else {
-            this.baseUrl = this.baseUrl + '8002';
-        }
+        // if (bParam.port) {
+        //     this.baseUrl = this.baseUrl + bParam.port
+        // } else {
+        //     this.baseUrl = this.baseUrl + '8002';
+        // }
 
-        // this.baseUrl = 'https://ttdb-test.dnvhot.tech';
+        this.baseUrl = 'https://ttdb-test.dnvhot.tech';
 
         // this.baseUrl = 'https://ttdb.dnvhot.tech';
 
