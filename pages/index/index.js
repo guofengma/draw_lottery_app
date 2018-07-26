0b94a35d63d09f9d5d82b94198b5cfe1df59b42a
weiweikate <690125575@qq.com>
2018-07-26 15:49:50 +0800

    修改代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -538,6 +538,9 @@ Page({
       this.setData({
         isNotice: !this.data.isNotice
       })
+      if (this.data.isNotice) {
+        this.selectComponent("#showNotice").noticeRequestHttp()
+      } 
     },
     showNotice: function (e) { // 显示公告
         this.setData({
