fe91d110a9cceffa9dcd640b456d1a851c434b1a
weiweikate <690125575@qq.com>
2018-08-08 09:27:48 +0800

    修改代码

--- pages/logistics/logistics.js
+++ pages/logistics/logistics.js
@@ -26,27 +26,27 @@ Page({
         r.finishBlock = (req) => {
             let datas = req.responseObject.data;
             if (datas) {
-              if (datas.showapi_res_body && datas.showapi_res_body.data){
-              
-                this.setData({
-                  img: datas.img,
-                  expTextName: datas.showapi_res_body.expTextName,
-                  mailNo: datas.showapi_res_body.mailNo,
-                  status: datas.showapi_res_body.status,
-                  phone: datas.phone
-                })
-                let list = datas.showapi_res_body.data;
-                let tempList = [];
-                if (list.length) {
-                  list.forEach((item) => {
-                    tempList.push(item)
-                  });
-                  this.setData({
-                    list: tempList
-                  })
+                if (datas.showapi_res_body) {
+                    this.setData({
+                        img: datas.img,
+                        expTextName: datas.showapi_res_body.expTextName,
+                        mailNo: datas.showapi_res_body.mailNo,
+                        status: datas.showapi_res_body.status,
+                        phone: datas.phone
+                    });
+                    if (datas.showapi_res_body.data) {
+                        let list = datas.showapi_res_body.data;
+                        let tempList = [];
+                        if (list.length) {
+                            list.forEach((item) => {
+                                tempList.push(item)
+                            });
+                            this.setData({
+                                list: tempList
+                            })
+                        }
+                    }
                 }
-              }
-               
             }
         };
         Tool.showErrMsg(r);
